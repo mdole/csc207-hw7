19530d57523cbc58146c81f2939ec78970d04430
John Brady <bradyjoh@grinnell.edu>
2013-10-31 13:53:00 -0500

    more commenting and Javadoc

--- src/edu/grinnell/csc207/dolematt/hw7/PartTwo.java
+++ src/edu/grinnell/csc207/dolematt/hw7/PartTwo.java
@@ -14,18 +14,30 @@ public class PartTwo {
      * @param client
      * @throws Exception
      */
-    public static DoublyLinkedList<UshahidiIncident> readIncidents(UshahidiClient client) throws Exception {
+    public static DoublyLinkedList<UshahidiIncident> readIncidents(
+	    UshahidiClient client) throws Exception {
 	DoublyLinkedList<UshahidiIncident> list = new DoublyLinkedList<UshahidiIncident>();
 	DoublyLinkedList<UshahidiIncident> list2 = new DoublyLinkedList<UshahidiIncident>();
-	DoublyLinkedListCursor<UshahidiIncident> c = new DoublyLinkedListCursor<UshahidiIncident>(list.dummy);
-	DoublyLinkedListCursor<UshahidiIncident> c2 = new DoublyLinkedListCursor<UshahidiIncident>(list2.dummy);
+	DoublyLinkedListCursor<UshahidiIncident> c = new DoublyLinkedListCursor<UshahidiIncident>(
+		list.dummy);
+	DoublyLinkedListCursor<UshahidiIncident> c2 = new DoublyLinkedListCursor<UshahidiIncident>(
+		list2.dummy);
 	while (client.hasMoreIncidents()) {
 	    list.insert(client.nextIncident(), c2);
-	    //list.append(client.nextIncident());
+	    // list.append(client.nextIncident());
 	} // while
 	return list;
     } // readIncidents(UshahidiClient)
 
+    /**
+     * Takes a list of UshahidiIncidents and calculates the average latitude and
+     * longitude of all the nodes, returning the averages as an UshahidiLocation
+     * 
+     * @param list
+     * @return avgLoc, an UshahidiLocation with the average latitude and
+     *         longitude of all locations in the list
+     * @throws Exception
+     */
     public static UshahidiLocation avgLoki(
 	    DoublyLinkedList<UshahidiIncident> list) throws Exception {
 	DoublyLinkedListCursor<UshahidiIncident> c = (DoublyLinkedListCursor<UshahidiIncident>) list
@@ -40,10 +52,20 @@ public class PartTwo {
 	    list.advance(c);
 	}
 	UshahidiLocation avgLoc = new UshahidiLocation(0,
-		"Average Longitude and Latitude", totalLat / counter, totalLong
-			/ counter);
+		"Average Longitude and Latitude", totalLat / counter,
+		totalLong / counter);
 	return avgLoc;
     }
+
+    /**
+     * takes a list of UshahidiIncidents and calls avgLoki to get the average
+     * location. It then returns a list of UshahidiIncidents that meets the
+     * predicate of being within 15 degrees of the average location.
+     * 
+     * @param list
+     * @return extracted, a list of UshahidiIncidents that fit pass the Predicate
+     * @throws Exception
+     */
 
     public static DoublyLinkedList<UshahidiIncident> extract(
 	    DoublyLinkedList<UshahidiIncident> list) throws Exception {
@@ -69,6 +91,19 @@ class LocPredicate implements Predicate<UshahidiIncident> {
     }
 
     // METHOD/PREDICATE
+    /**
+     * creates a predicate that tests if an UshahidiIncident's location is
+     * within 15 degrees of the input location
+     * 
+     * @param val
+     * @return (dist <= 15), a boolean
+     * @pre val is an UshahidiIncident
+     * @post returns true if the UshahidiIncident is within 15 degrees of the
+     *       input UshahidiIncident. returns false if the UshahidiIncident does
+     *       not fall within that 15 degree radius circle around the input
+     *       UshahidiIncident.
+     */
+
     @Override
     public boolean test(UshahidiIncident val) {
 	double dist = Math.sqrt(Math.pow(val.getLocation().getLatitude()
@@ -79,6 +114,5 @@ class LocPredicate implements Predicate<UshahidiIncident> {
 	return dist <= 15;
     }
 
- 
 } // class LocPredicate
 
